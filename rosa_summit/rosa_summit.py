978631706198aa006ebc08b036e4dd6cd8750a05
Giovanni Toffetti <toff@zhaw.ch>
2025-03-21 07:55:36 +0000

    Added stop tool to simplify halting invocation

--- rosa_summit/rosa_summit.py
+++ rosa_summit/rosa_summit.py
@@ -2,21 +2,35 @@ from langchain_ollama import ChatOllama
 from langchain.agents import tool
 from rosa import ROSA
 from rosa.tools.ros2 import execute_ros_command
+from rosa.prompts import RobotSystemPrompts
+
 
 @tool
 def send_vel(velocity: float) -> str:
     """
-    Sets the forward velocity of the robot
+    Sets the forward velocity of the robot.
     
     :param velocity: the velocity at which the robot should move
     """
-    # Your code here ...
     cmd = "ros2 topic pub -1 /summit/cmd_vel geometry_msgs/msg/Twist '{linear: {x: " + str(velocity) + ", y: 0.0, z: 0.0}, angular: { x: 0.0, y: 0.0, z: 0.0}}'"
     success, output = execute_ros_command(cmd)
     if success:
         return "Velocity set to %s" % velocity
     else:
         return "Failed setting velocity"
+    
+@tool
+def stop() -> str:
+    """
+    Stops or halts the robot by setting its velocity to zero
+
+    """
+    cmd = "ros2 topic pub -1 /summit/cmd_vel geometry_msgs/msg/Twist '{linear: {x: 0.0, y: 0.0, z: 0.0}, angular: { x: 0.0, y: 0.0, z: 0.0}}'"
+    success, output = execute_ros_command(cmd)
+    if success:
+        return "Robot was stopped"
+    else:
+        return "Failed stopping robot"
 
 def main():
     print('Hi from rosa_summit.')
@@ -27,8 +41,11 @@ def main():
         base_url="http://160.85.252.236:11434",
     )
 
+    prompt = RobotSystemPrompts()
+    prompt.critical_instructions = "Before using a tool that performs an action, respond by saying which tool will be invoked and with which parameters. Always ask for confirmation before calling the tool."
+
     # Pass the LLM to ROSA
-    agent = ROSA(ros_version=2, llm=ollama_llm, tools=[send_vel])
+    agent = ROSA(ros_version=2, llm=ollama_llm, tools=[send_vel], prompts=prompt)
 
     while True:
         msg = input("Enter your request: ")
