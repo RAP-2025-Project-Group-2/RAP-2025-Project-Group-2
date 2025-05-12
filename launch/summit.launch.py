329841ec97a2994f833fdbd1b8787180ea79d3fc
Alexander Kolenaty <akolenaty@protonmail.ch>
2025-05-12 15:38:33 +0200

    also launch navigation and slam

--- launch/summit.launch.py
+++ launch/summit.launch.py
@@ -14,6 +14,13 @@ def generate_launch_description():
         "summit_xl_simulation_ign.launch.py",
     )
 
+    # Path to the navigation launch file
+    summit_xl_nav2_launch_file = os.path.join(
+        FindPackageShare("icclab_summit_xl").find("icclab_summit_xl"),
+        "launch",
+        "summit_xl_nav2.launch.py",
+    )
+
     # Declare the launch arguments for the included launch file (if any)
     # For example, if summit_xl_simulation_ign.launch.py accepts 'world_name':
     # summit_xl_launch_args = {
@@ -26,6 +33,11 @@ def generate_launch_description():
             IncludeLaunchDescription(
                 PythonLaunchDescriptionSource(summit_xl_simulation_launch_file),
                 # launch_arguments=summit_xl_launch_args  # Uncomment if you have launch arguments
+            ),
+            # Include the Summit XL navigation launch file with SLAM enabled
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(summit_xl_nav2_launch_file),
+                launch_arguments={"slam": "True"}.items(),
             ),
         ]
     )
