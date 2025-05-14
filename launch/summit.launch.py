2416a76d650b719478ff9955539ed9dbfe761f69
Alexander Kolenaty <akolenaty@protonmail.ch>
2025-05-14 22:08:36 +0200

    map loading and slam toggle

--- launch/summit.launch.py
+++ launch/summit.launch.py
@@ -1,12 +1,24 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, ExecuteProcess
+from launch.actions import (
+    IncludeLaunchDescription,
+    ExecuteProcess,
+    DeclareLaunchArgument,
+)
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import LaunchConfiguration
 import os
 
 
 def generate_launch_description():
+    # --- Declare and configure launch arguments ---
+    declare_slam_arg = DeclareLaunchArgument(
+        "slam",
+        default_value="False",  # Default is 'False' (string)
+        description="Enable SLAM. If True, SLAM is used. If False, a pre-existing map is used.",
+    )
+    slam = LaunchConfiguration("slam") == "True"
+
     # Path to the launch file from icclab_summit_xl package
     summit_xl_simulation_launch_file = os.path.join(
         FindPackageShare("icclab_summit_xl").find("icclab_summit_xl"),
@@ -47,24 +59,50 @@ def generate_launch_description():
         )
     )
 
-    # Include the Summit XL navigation launch file with SLAM enabled
-    launch_description.append(
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(summit_xl_nav2_launch_file),
-            launch_arguments={"slam": "True"}.items(),
+    if slam:
+        # Include the Summit XL navigation launch file with SLAM enabled
+        launch_description.append(
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(summit_xl_nav2_launch_file),
+                launch_arguments={"slam": "True"}.items(),
+            )
         )
-    )
 
-    # Include the explore_lite launch file
-    launch_description.append(
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(explore_lite_launch_file),
-            launch_arguments={
-                "namespace": "/summit",
-                "use_sim_time": "true",
-            }.items(),
+        # Include the explore_lite launch file
+        launch_description.append(
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(explore_lite_launch_file),
+                launch_arguments={
+                    "namespace": "/summit",
+                    "use_sim_time": "true",
+                }.items(),
+            )
         )
-    )
+
+        # Command to stop exploration immediately after starting
+        launch_description.append(
+            ExecuteProcess(
+                cmd=[
+                    "ros2",
+                    "topic",
+                    "pub",
+                    "--once",
+                    "/summit/explore/resume",
+                    "std_msgs/msg/Bool",
+                    "{data: false}",
+                ],
+            )
+        )
+    else:
+        # Include the Summit XL navigation launch file with SLAM enabled
+        launch_description.append(
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(summit_xl_nav2_launch_file),
+                launch_arguments={
+                    "map": "/home/ros/rap/Gruppe2/maps/default.yaml"
+                }.items(),
+            )
+        )
 
     # Include the Summit XL MoveIt launch file
     # launch_description.append(
@@ -72,20 +110,5 @@ def generate_launch_description():
     #         PythonLaunchDescriptionSource(summit_xl_move_it_launch_file),
     #     )
     # )
-
-    # Command to stop exploration immediately after starting
-    launch_description.append(
-        ExecuteProcess(
-            cmd=[
-                "ros2",
-                "topic",
-                "pub",
-                "--once",
-                "/summit/explore/resume",
-                "std_msgs/msg/Bool",
-                "{data: false}",
-            ],
-        )
-    )
 
     return LaunchDescription(launch_description)
