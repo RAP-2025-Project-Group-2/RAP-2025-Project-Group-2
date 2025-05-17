e3e92cd19afcab6affd3a21b31b0a3fb44ceac7a
Alexander Kolenaty <akolenaty@protonmail.ch>
2025-05-17 08:47:36 +0200

    fix exploration not launching

--- launch/summit.launch.py
+++ launch/summit.launch.py
@@ -26,18 +26,18 @@ def generate_launch_description():
         "summit_xl_simulation_ign.launch.py",
     )
 
-    # Path to the navigation launch file
-    summit_xl_nav2_launch_file = os.path.join(
-        FindPackageShare("icclab_summit_xl").find("icclab_summit_xl"),
-        "launch",
-        "summit_xl_nav2.launch.py",
-    )
-
     # Path to the MoveIt launch file
     summit_xl_move_it_launch_file = os.path.join(
         FindPackageShare("icclab_summit_xl").find("icclab_summit_xl"),
         "launch",
         "summit_xl_move_it.launch.py",
+    )
+
+    # Path to the navigation launch file
+    summit_xl_nav2_launch_file = os.path.join(
+        FindPackageShare("icclab_summit_xl").find("icclab_summit_xl"),
+        "launch",
+        "summit_xl_nav2.launch.py",
     )
 
     # Path to the explore_lite launch file
@@ -47,68 +47,59 @@ def generate_launch_description():
         "explore.launch.py",
     )
 
-    launch_description = []
-
-    # Include the Summit XL simulation launch file
-    launch_description.append(
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(summit_xl_simulation_launch_file),
-            launch_arguments={
-                "world": "/home/bonsai/Development/rap/Gruppe2/world/small_house.world"
-            }.items(),
-        )
-    )
-
     if slam:
-        # Include the Summit XL navigation launch file with SLAM enabled
-        launch_description.append(
-            IncludeLaunchDescription(
-                PythonLaunchDescriptionSource(summit_xl_nav2_launch_file),
-                launch_arguments={"slam": "True"}.items(),
-            )
-        )
-
-        # Include the explore_lite launch file
-        launch_description.append(
-            IncludeLaunchDescription(
-                PythonLaunchDescriptionSource(explore_lite_launch_file),
-                launch_arguments={
-                    "namespace": "/summit",
-                    "use_sim_time": "true",
-                }.items(),
-            )
-        )
-
-        # Command to stop exploration immediately after starting
-        launch_description.append(
-            ExecuteProcess(
-                cmd=[
-                    "ros2",
-                    "topic",
-                    "pub",
-                    "--once",
-                    "/summit/explore/resume",
-                    "std_msgs/msg/Bool",
-                    "{data: false}",
-                ],
-            )
+        return LaunchDescription(
+            [
+                # Include the Summit XL simulation launch file
+                IncludeLaunchDescription(
+                    PythonLaunchDescriptionSource(summit_xl_simulation_launch_file),
+                    launch_arguments={
+                        "world": "/home/bonsai/Development/rap/Gruppe2/world/small_house.world"
+                    }.items(),
+                ),
+                # Include the Summit XL navigation launch file with SLAM enabled
+                IncludeLaunchDescription(
+                    PythonLaunchDescriptionSource(summit_xl_nav2_launch_file),
+                    launch_arguments={"slam": "True"}.items(),
+                ),
+                # Include the explore_lite launch file
+                IncludeLaunchDescription(
+                    PythonLaunchDescriptionSource(explore_lite_launch_file),
+                    launch_arguments={
+                        "namespace": "/summit",
+                        "use_sim_time": "True",
+                    }.items(),
+                ),
+                # Command to stop exploration immediately after starting
+                ExecuteProcess(
+                    cmd=[
+                        "ros2",
+                        "topic",
+                        "pub",
+                        "--once",
+                        "/summit/explore/resume",
+                        "std_msgs/msg/Bool",
+                        "{data: false}",
+                    ],
+                ),
+            ]
         )
     else:
-        # Include the Summit XL navigation launch file with SLAM enabled
-        launch_description.append(
-            IncludeLaunchDescription(
-                PythonLaunchDescriptionSource(summit_xl_nav2_launch_file),
-                launch_arguments={
-                    "map": "/home/ros/rap/Gruppe2/maps/default.yaml"
-                }.items(),
-            )
+        return LaunchDescription(
+            [
+                # Include the Summit XL simulation launch file
+                IncludeLaunchDescription(
+                    PythonLaunchDescriptionSource(summit_xl_simulation_launch_file),
+                    launch_arguments={
+                        "world": "/home/bonsai/Development/rap/Gruppe2/world/small_house.world"
+                    }.items(),
+                ),
+                # Include the Summit XL navigation launch file with SLAM enabled
+                IncludeLaunchDescription(
+                    PythonLaunchDescriptionSource(summit_xl_nav2_launch_file),
+                    launch_arguments={
+                        "map": "/home/ros/rap/Gruppe2/maps/default.yaml"
+                    }.items(),
+                ),
+            ]
         )
-
-    # Include the Summit XL MoveIt launch file
-    # launch_description.append(
-    #     IncludeLaunchDescription(
-    #         PythonLaunchDescriptionSource(summit_xl_move_it_launch_file),
-    #     )
-    # )
-
-    return LaunchDescription(launch_description)
